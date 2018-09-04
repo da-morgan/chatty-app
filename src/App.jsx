93d738d76da4dcff084a47dddad831fe2664a0eb
David Morgan <david.alexandermorgan@gmail.com>
2018-09-04 18:52:06 +0000

    Refactored so that messages array is state in the App component. Passing relevant data down as Props. Added Key to the messages array. Dummy data rendering correctly

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,62 @@ import MessageList from "./MessageList.jsx"
 
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      currentUser: "anonymous",
+      messages:[
+        {
+          id: 1,
+          type: "incomingMessage",
+          content: "I won't be impressed with technology until I can download food.",
+          username: "Anonymous1"
+        },
+        {
+          id: 2,
+          type: "incomingNotification",
+          content: "Anonymous1 changed their name to nomnom",
+        },
+        {
+          id: 3,
+          type: "incomingMessage",
+          content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+          username: "Anonymous2"
+        },
+        {
+          id: 4,
+          type: "incomingMessage",
+          content: "...",
+          username: "nomnom"
+        },
+        {
+          id: 5,
+          type: "incomingMessage",
+          content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+          username: "Anonymous2"
+        },
+        {
+          id: 6,
+          type: "incomingMessage",
+          content: "This isn't funny. You're not funny",
+          username: "nomnom"
+        },
+        {
+          id: 7,
+          type: "incomingNotification",
+          content: "Anonymous2 changed their name to NotFunny",
+        },
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
         <NavBar />
-        <MessageList />
-        <ChatBar />
+        <MessageList key={this.state.messages.id} messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor(props){
+    super()
+
+  }
+
+  changeableUsername
+
   render() {
     return (
         <footer className="chatbar">
-            <input className="chatbar-username" placeholder="Your Name (Optional)" />
+            <input className="chatbar-username" placeholder={this.props.currentUser} />
             <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
         </footer>
     );
